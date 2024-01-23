4f2a91cc54cebf3470884eccf7fa8ca786eb819a
TorresTatsuya <150040285+TorresTatsuya@users.noreply.github.com>
2024-01-23 22:24:26 +0900

    機能追加: 背景が下に流れるように機能を追加

---
+++ Assets/Scripts/BackGroundCtl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundCtl : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    void FixedUpdate(){
+        if (transform.position.y <= -20.5f){
+            transform.position = new Vector3(transform.position.x, 0f, 0f);
+            Debug.Log("リセットされました");
+        }
+        transform.position += Vector3.down * Time.deltaTime * moveSpeed;
+    }
+}
